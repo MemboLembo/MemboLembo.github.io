9b1042456eaf6080d1d7d46d7b9fc6be579c1a1f
MemboLembo <MemboLembo@users.noreply.github.com>
2020-02-08 13:51:34 +0000

    deploy: MemboLembo/Anime-search@89b10e9a298474231b7058f7725133a09b7286eb

--- anime/js/pages/home.js
+++ anime/js/pages/home.js
@@ -3,6 +3,13 @@ import { loadingAnimationToggle } from '../dom-utils.js';
 import { setInputVal } from '../dom-utils.js';
 import { randomAnime } from '../random-anime.js';
 
+// Loading JS takes some time. We disable the search
+// button to not let it refresh the page
+// until we initialize its click handler
+function enableSearchButton () {
+  const button = document.querySelector('.search-form__content__btn');
+  button.removeAttribute('disabled');
+}
 
 export const initHome = () => {
   const userSearch = document.querySelector('.search-form__content__input');
@@ -12,7 +19,7 @@ export const initHome = () => {
   randomButton.addEventListener('click', function() {
     loadingAnimationToggle("display: block;");
     randomAnime();
-  })
+  });
 
   searchButton.addEventListener('click', function (e) {
     e.preventDefault();
@@ -25,6 +32,7 @@ export const initHome = () => {
       goTo('search/' + searchVar + '/nsfw');
     }
   });
+  enableSearchButton();
 };
 
 export default () => {
