fc4efe7e4d0ecbd50f0a42dd10e0b4437948ab76
MemboLembo <MemboLembo@users.noreply.github.com>
2020-03-12 18:24:21 +0000

    deploy: MemboLembo/react-todo-hook@bace7813c9b0da37f3dae34686d780c5fa7eee00

---
+++ react-todo-hook/precache-manifest.701ab530906b3c8cbdfb68bd0e5a4b07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b086df71fb651a109a4286ee42094480",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8d9a69cf98270790ac9a",
+    "url": "./static/css/main.94841b98.chunk.css"
+  },
+  {
+    "revision": "915da98131b7927011fd",
+    "url": "./static/js/2.9df413db.chunk.js"
+  },
+  {
+    "revision": "fd7ef187897c97dde29bc52fd1e82e09",
+    "url": "./static/js/2.9df413db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8239a7396ed33bd0c8c7",
+    "url": "./static/js/3.9a40824c.chunk.js"
+  },
+  {
+    "revision": "8d9a69cf98270790ac9a",
+    "url": "./static/js/main.0d4054df.chunk.js"
+  },
+  {
+    "revision": "c035577c42c57a20ecaf",
+    "url": "./static/js/runtime-main.3c513bbd.js"
+  }
+]);
