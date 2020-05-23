c1565556572680625e5f31b847f9a4116c1995c8
MemboLembo <MemboLembo@users.noreply.github.com>
2020-05-23 18:28:21 +0000

    deploy: MemboLembo/starwars@b0483502b5fe69c45c6c0946baebae4f4ebc3b7f

---
+++ starwars/precache-manifest.065333f2c34d68349e0acf3f3aff67b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04637a1a6fe39d56c67fa718c4c1abf8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a3abcefd4b4dbd38788",
+    "url": "/static/css/main.a6e2a475.chunk.css"
+  },
+  {
+    "revision": "cbc64ce1805de851912d",
+    "url": "/static/js/2.5389dd23.chunk.js"
+  },
+  {
+    "revision": "0f2be52aac348a57b5e1722f475cbfd0",
+    "url": "/static/js/2.5389dd23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a3abcefd4b4dbd38788",
+    "url": "/static/js/main.f51b33f7.chunk.js"
+  },
+  {
+    "revision": "2b666024c89226609676",
+    "url": "/static/js/runtime-main.285bcf4f.js"
+  }
+]);
