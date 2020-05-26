c2835cc73281a3e6201c96fac5a1102a60c5d2ec
MemboLembo <MemboLembo@users.noreply.github.com>
2020-05-26 12:06:15 +0000

    deploy: MemboLembo/redux-sandbox@334df4887f976fed990a7ab30cf2d586c3e3dbd7

---
+++ redux-sandbox/precache-manifest.7bee69134e03a24a81dae999bc73b018.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a8b28b5aa672e7d898e716c4c5c98e1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b4af4dbcdf37db766a40",
+    "url": "./static/js/2.f579a802.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.f579a802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98074d20d29c10f1d11a",
+    "url": "./static/js/main.6e8774ec.chunk.js"
+  },
+  {
+    "revision": "96f97083a09f565dfe62",
+    "url": "./static/js/runtime-main.dc85d4ef.js"
+  }
+]);
