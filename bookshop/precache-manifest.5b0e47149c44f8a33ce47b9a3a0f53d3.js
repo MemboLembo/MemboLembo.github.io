44d4feb60bbd658fedf5098bbd857a7d66ec22b6
MemboLembo <MemboLembo@users.noreply.github.com>
2020-05-28 23:00:48 +0000

    deploy: MemboLembo/bookshop@a0295091e6a855256347f233a016bf789cc69c1b

---
+++ bookshop/precache-manifest.5b0e47149c44f8a33ce47b9a3a0f53d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e19d30c2d12856a6788b3b0fa40d2df4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32738f5ad0d696f26ed0",
+    "url": "/static/css/main.8317bb71.chunk.css"
+  },
+  {
+    "revision": "76a3b9f048642ae63334",
+    "url": "/static/js/2.a17cf564.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a17cf564.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32738f5ad0d696f26ed0",
+    "url": "/static/js/main.7334f59d.chunk.js"
+  },
+  {
+    "revision": "a328250fd09aed6f66d8",
+    "url": "/static/js/runtime-main.3413e37a.js"
+  }
+]);
